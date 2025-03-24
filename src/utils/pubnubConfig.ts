e2fd8803c12d87ace4b2112a6b7f7588b09a269f
ijforkuo <ignatus373@gmail.com>
2025-03-24 15:45:16 +0000

    Removed .env and switched to Expo Constants

--- src/utils/pubnubConfig.ts
+++ src/utils/pubnubConfig.ts
@@ -1,6 +1,7 @@
 import PubNub from 'pubnub';
 import Constants from "expo-constants";
 
+
 const pubnub = new PubNub({
   publishKey: Constants.expoConfig?.extra?.PUBNUB_PUBLISH_KEY as string,
   subscribeKey: Constants.expoConfig?.extra?.PUBNUB_SUBSCRIBE_KEY as string,
